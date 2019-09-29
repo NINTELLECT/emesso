2924315b56eeded5a563426ce9813078ade928b3
Brian Mutiso <brianamutiso@gmail.com>
2019-09-29 12:51:10 +0300

    Implementing a Slideable Widget Using Bottomsheet in Flutter

--- lib/pages/ConversationPage.dart
+++ lib/pages/ConversationPage.dart
@@ -1,26 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:nintellect/widgets/ChatAppBar.dart';
 import 'package:nintellect/widgets/ChatListWidget.dart';
+import 'package:nintellect/widgets/ConversationBottomSheet.dart';
 import 'package:nintellect/widgets/InputWidget.dart';
 
 
 class ConversationPage extends StatefulWidget {
   @override
   _ConversationPageState createState() => _ConversationPageState();
+  const ConversationPage();
 }
 
 class _ConversationPageState extends State<ConversationPage> {
 
+ final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+          key: _scaffoldKey,
           appBar: ChatAppBar(), // Custom app bar for chat screen
           body: Stack(children: <Widget>[
             Column(
               children: <Widget>[
               ChatListWidget(),//Chat list 
-                InputWidget() // The input widget
+                 GestureDetector(
+                      child: InputWidget(),
+                      onPanUpdate: (details) {
+                        if (details.delta.dy <0) {
+                          _scaffoldKey.currentState
+                              .showBottomSheet<Null>((BuildContext context) {
+                            return ConversationBottomSheet();
+                          });
+                        }
+                      })
+
               ],
             ),
           ]
